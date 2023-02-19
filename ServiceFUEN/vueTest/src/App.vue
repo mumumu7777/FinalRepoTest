eaf6ffea6fafe41c379dff32ee69126cee048d38
DESKTOP-E1P73N6\kevin <kevin3571630@gmail.com>
2023-02-19 22:43:23 +0800

    VueChange

---
+++ ServiceFUEN/vueTest/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <div style="font-size: 3em;" @click="vueTest()">Vue測試!!!!!</div>
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  methods: {
+    vueTest(){
+      alert("你好")
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ ServiceFUEN/vueTest/vue.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  outputDir: "../wwwroot",
+  devServer: {
+    open: true,
+    host: 'localhost',  // 設置主機地址
+    port: 8080,
+    proxy: {
+      '/api': {
+        target: 'https://localhost:7259',
+        ws: true,     // 如果要代理 websockets
+        secure: false,
+        changOrigin: true,  //允許跨域
+        // logLevel: "debug"
+        // pathRewrite: {
+        //   '^/api': '/api',
+        // }
+      },
+    },
+  },
+  lintOnSave: false, //关闭eslint检查
+  // publicPath: process.env.VUE_APP_PATH,
+  productionSourceMap: false,
+  filenameHashing: false,
+  // css: {
+  //   extract: false
+  // },
+}
